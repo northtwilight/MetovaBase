101592a4652d4bd510bf36578d0bf2aefa427e93
Nick Griffith <nick.griffith@metova.com>
2016-04-25 07:11:05 -0500

    - adjusting .swiftlint.yml to enforce doc comments  - updating CHANGELOG  - adjusting indent settings on some of the .yml files  - adding first responder tests
    The `testResignsResponder` test is failing.  The text field simply never reports being first responder in the first place (line 55 fails).

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # MetovaBase CHANGELOG
 
+## 0.2.0
+
+Renamed to MetovaBase.
+
 ## 0.1.0
 
 Initial release.
--- MetovaBaseTests/BaseViewControllerTests/BaseViewControllerFirstResponderTests.swift
+++ MetovaBaseTests/BaseViewControllerTests/BaseViewControllerFirstResponderTests.swift
@@ -12,6 +12,50 @@ import XCTest
 
 class BaseViewControllerFirstResponderTests: XCTestCase {
 
-// TODO: Write tests around `dismissKeyboard()` method.
+    func testControllerNoResponders() {
+       
+        class NoRespondersController: BaseViewController {}
+        
+        let testObject = NoRespondersController()
+        testObject.loadView()
+        testObject.viewDidLoad()
+        
+        BCTAssertNoException {
+            testObject.dismissKeyboard()
+        }
+    }
 
+    func testResignsResponder() {
+        
+        class HasTextFieldController: BaseViewController {
+            
+            var textField: UITextField?
+            
+            override func viewDidLoad() {
+                super.viewDidLoad()
+                
+                let textField = UITextField()
+                view.addSubview(textField)
+                UIWindow().addSubview(view)
+                self.textField = textField
+            }
+        }
+        
+        let testObject = HasTextFieldController()
+        testObject.loadView()
+        testObject.viewDidLoad()
+        
+        guard let textField = testObject.textField else {
+            XCTFail("Failed to obtain text field reference on test controller.")
+            return
+        }
+        
+        textField.becomeFirstResponder()
+        
+        XCTAssertTrue(textField.isFirstResponder())
+        
+        testObject.dismissKeyboard()
+        
+        XCTAssertFalse(textField.isFirstResponder())
+    }
 }
